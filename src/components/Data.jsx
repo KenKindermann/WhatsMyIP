be54e830465e1d253aa5822413092896b0924e7a
kenkindermann <ken.kindermann@gmail.com>
2024-02-18 20:41:16 +0100

    implement country section

--- src/components/Data.jsx
+++ src/components/Data.jsx
@@ -1,10 +1,12 @@
 // Components
 import Ip from "./Ip";
+import Country from "./Country";
 
 const Data = () => {
   return (
     <section className="flex flex-col items-center lg:flex-row  mb-8 gap-8">
       <Ip />
+      <Country />
     </section>
   );
 };
