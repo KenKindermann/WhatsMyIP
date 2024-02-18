21e6ccfab1679b87764c1416ba1ebcf5f2da4e2b
kenkindermann <ken.kindermann@gmail.com>
2024-02-18 17:37:05 +0100

    implement date section

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+// CSS
 import "./App.css";
 
+// Components
+import Main from "./components/Main";
+
 function App() {
-  return <></>;
+  return (
+    <div className="bg-slate-800 text-white py-4 px-8">
+      <Main />
+    </div>
+  );
 }
 
 export default App;
